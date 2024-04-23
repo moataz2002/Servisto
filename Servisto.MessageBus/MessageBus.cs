c521faf42709067954243dad2d180544d33e3dac
moataz2002 <mezzo.m.2030@gmail.com>
2024-04-23 16:50:34 +0200

    New Modifications

--- Servisto.MessageBus/MessageBus.cs
+++ Servisto.MessageBus/MessageBus.cs
@@ -11,7 +11,7 @@ namespace Servisto.MessageBus
     public class MessageBus : IMessageBus
     {
 
-        private string connectionString = "Endpoint=sb://Servistoweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HjoslS58pPHtAULb0tay/jx4Ys0+MO5/R+ASbCcFTG0=";
+        private string connectionString = "YourConnection";
 
         public async Task PublishMessage(object message, string topic_queue_Name)
         {
